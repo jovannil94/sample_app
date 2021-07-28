c1bbc535616e7bd5c95055bf54da620da4c50bac
Jovanni Luna <jluna@foursquare.com>
2021-07-28 18:17:03 -0400

    fixed test statick_pages_controllers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,27 +1,13 @@
-require 'test_helper'
-
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
-  test 'should get home' do
-    get static_pages_home_url
-    assert_response :success
-    assert_select 'title', 'Ruby on Rails Tutorial Sample App'
+class StaticPagesController < ApplicationController
+  def home
   end
 
-  test 'should get help' do
-    get static_pages_help_url
-    assert_response :success
-    assert_select 'title', 'Help | Ruby on Rails Tutorial Sample App'
+  def help
   end
 
-  test 'should get about' do
-    get static_pages_about_url
-    assert_response :success
-    assert_select 'title', 'About | Ruby on Rails Tutorial Sample App'
+  def about
   end
 
-  test 'should get contact' do
-    get static_pages_contact_url
-    assert_response :success
-  end 
+  def contact
+  end
 end
